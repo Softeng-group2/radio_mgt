19f3aeb7e60ee149e5ff954a1be4527cb1de3282
jamesopuni <jayaimsopuni@gmail.com>
2017-03-20 15:56:17 +0000

    Add files via upload
    it shows the attendance records

---
+++ attendance.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Attendane</title>
+<style>
+body{
+	background-size:cover;
+
+}
+</style>
+</head>
+
+<body background="images/miocrosoft_surface-1280x800 (1).jpg">
+
+</body>
+</html>
